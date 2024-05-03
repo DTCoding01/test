0c9cfef841a562efc602601d128cb910f9ccccb6
Daniel Taylor <danieljtaylor153@gmail.com>
2024-05-03 20:28:48 +0100

    first commit

---
+++ groceries.py
@@ -0,0 +1,16 @@
+eggs = 2
+banana = 1
+salad = 10
+_hello = 2123
+steak_and_rice = 30
+
+print("Breakfast Cost:")
+print(eggs + banana)
+
+print("Lunch Cost:")
+print(salad)
+
+print("Dinner Cost:")
+print(steak_and_rice)
+
+print(_hello)
